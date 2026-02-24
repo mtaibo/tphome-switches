b989df15560c42fedaa534c4331a3ebe9e4d2882
Miguel Taibo <miguel.taibo@icloud.com>
2026-02-24 12:00:48 +0100

    Few modifications on default.h to make it clearer, with specifications about measures

--- include/default.h
+++ include/default.h
@@ -1,32 +1,42 @@
 #ifndef DEFAULT_H
 #define DEFAULT_H
 
+// ~~~~~~~~~~~~~~~~~~~~         Measures         ~~~~~~~~~~~~~~~~~~~~~~~
+//
+//  Variables with this measures, time or pos are 16 bits unsigned ints
+//
+// Time      -  Measured in centiseconds (1.00s = 100   | 25.12s = 2512)
+// Position  -  Measured                 (10.00% = 1000 | 26.37% = 2637)
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 // ------- WiFi / MQTT Credentials  ---------
 
-#include "credentials.h"
+#include "credentials.h" // Only a credentials.example.h is on this repository to prevent showing private passwords
 
 // ---------     Identification     ---------
 
-#define DEF_DEVICE_ID               "B364859"
-#define DEF_ROOM          "secondary-bedroom"
+#define DEF_DEVICE_ID               "B364859" // This device ID must follow a standard, beginning with B for Blind or L for Light, and having 6 numbers after that letter
+#define DEF_ROOM                    "bedroom"
 #define DEF_NAME                      "blind"
 
 // ---------     Preferences        ---------
 
-#define DEF_UP_TIME                     30000
-#define DEF_DOWN_TIME                   30000
-#define DEF_DOWN_POSITION                  20 // This value is used to partially close the blind to a memorised position
+#define DEF_UP_TIME                      4000 // 40.00 seconds
+#define DEF_DOWN_TIME                    4000 // 40.00 seconds
+#define DEF_DOWN_POSITION                2000 // 20%              -   This value is used to partially close the blind to a memorised position
 
-#define DEF_MOTOR_SAFE_TIME              1000 // This value is a safe time for the motor between going up and down
-#define DEF_STOP_LED_TIME                 500
+#define DEF_MOTOR_SAFE_TIME              0100 //  1.00 seconds    -   This value is a safe time for the motor between going up and down
+#define DEF_STOP_LED_TIME                0050 //  0.50 seconds
 
-#define DEF_SHORT_PULSE                  3000
-#define DEF_LONG_PULSE                  10000
+#define DEF_SHORT_PULSE                  0300 //  3.00 seconds
+#define DEF_LONG_PULSE                   1000 // 10.00 seconds
 
 // ---------     Default State      ---------
 
-#define DEF_CURRENT_POSITION              100
-#define DEF_NEXT_POSITION                 100
+#define DEF_CURRENT_POSITION             5000 // 50%
+#define DEF_NEXT_POSITION                5000 // 50%
 
 #define DEF_ACTIVE_RELAY               NO_PIN
 #define DEF_ACTIVE_LED                 NO_PIN
