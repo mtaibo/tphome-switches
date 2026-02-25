0b7b0c23fe0ddcf673de673fb76ea7a11974d9d8
Miguel Taibo <miguel.taibo@icloud.com>
2026-02-25 17:24:09 +0100

    Major change, new file distribution to make it scalable for the future implementations

---
+++ include/core/credentials.h
@@ -0,0 +1,16 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+// ---------      WiFi     ------------
+
+#define WIFI_SSID           "WIFI-TP24"
+#define WIFI_PASS     "S36M28LKLiAB13Z"
+
+// ---------      MQTT     ------------
+
+#define DEF_MQTT_IP     "192.168.1.160"
+#define DEF_MQTT_PORT              1883
+#define DEF_MQTT_USER                ""
+#define DEF_MQTT_PASS                ""
+
+#endif
