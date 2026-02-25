82b4a3c8baf927e069074738609dacadbcd4c5ca
Miguel Taibo <miguel.taibo@icloud.com>
2026-02-25 17:48:14 +0100

    Modified pins.h to adecuate them to the new folder and hardware division

--- include/hardware/esp32/pins.h
+++ include/hardware/esp32/pins.h
@@ -1,38 +1,41 @@
 #ifndef PINS_H
 #define PINS_H
 
-#include <Arduino.h>
+#include <stdint.h> // Lighter library than Arduino just for variable types
 
 // ~~~~~~~~~~~~~~~~~~~~        Hardware Logic        ~~~~~~~~~~~~~~~~~~~~
 
-static constexpr uint8_t LED_ON          = HIGH;
-static constexpr uint8_t LED_OFF         = LOW;
+namespace HW_Logic {
 
-static constexpr uint8_t LED_RED         = HIGH;
-static constexpr uint8_t LED_BLUE        = LOW;
+  static constexpr uint8_t LED_ACTIVE   = HIGH;
+  static constexpr uint8_t LED_IDLE     = LOW;
 
-static constexpr uint8_t BUTTON_PRESSED  = LOW;
-static constexpr uint8_t BUTTON_RELEASED = HIGH;
+  static constexpr uint8_t BTN_ACTIVE   = LOW;
+  static constexpr uint8_t BTN_IDLE     = HIGH;
 
-static constexpr uint8_t RELAY_ON        = HIGH;
-static constexpr uint8_t RELAY_OFF       = LOW;
+  static constexpr uint8_t RELAY_ACTIVE = HIGH;
+  static constexpr uint8_t RELAY_IDLE   = LOW;
+}
 
 // ~~~~~~~~~~~~~~~~~~~~            Pinout            ~~~~~~~~~~~~~~~~~~~~
 
-// --- LEDS ---
-static constexpr uint8_t PIN_LED_CONFIG = 1;  // Shared with TX/RX - Set this led to OUTPUT mode and HIGH to activate blue leds, and LOW or INPUT mode to deactivate blue leds
-static constexpr uint8_t PIN_LED_GREEN  = 0;
-static constexpr uint8_t PIN_LED_BOTTOM = 2;
-static constexpr uint8_t PIN_LED_MID    = 14;
-static constexpr uint8_t PIN_LED_TOP    = 16;
+namespace Pins {
 
-// --- BUTTONS ---
-static constexpr uint8_t PIN_BTN_BOTTOM = 5;
-static constexpr uint8_t PIN_BTN_MID    = 3;  // Shared with TX/RX
-static constexpr uint8_t PIN_BTN_TOP    = 12;
+  // --- LEDS ---
+  static constexpr uint8_t LED_CONFIG = 1;  // Shared with TX/RX - Set this led to OUTPUT mode and HIGH to activate blue leds, and LOW or INPUT mode to deactivate blue leds
+  static constexpr uint8_t LED_GREEN  = 0;
+  static constexpr uint8_t LED_BTM    = 2;
+  static constexpr uint8_t LED_MID    = 14;
+  static constexpr uint8_t LED_TOP    = 16;
 
-// --- RELAYS ---
-static constexpr uint8_t PIN_RELAY_UP   = 13; // Output L1
-static constexpr uint8_t PIN_RELAY_DOWN = 15; // Output L3
+  // --- BUTTONS ---
+  static constexpr uint8_t BTN_BTM    = 5;
+  static constexpr uint8_t BTN_MID    = 3;  // Shared with TX/RX
+  static constexpr uint8_t BTN_TOP    = 12;
+
+  // --- RELAYS ---
+  static constexpr uint8_t RELAY_UP   = 13; // Output L1
+  static constexpr uint8_t RELAY_DOWN = 15; // Output L3
+}
 
 #endif // PINS_H
